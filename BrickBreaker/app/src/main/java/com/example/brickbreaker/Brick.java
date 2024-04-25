01fa8a7c33888ca1971b5cf35e0436ab387846a1
ReneSotoCal <rs11659@georgiasouthern.edu>
2024-04-25 18:29:38 -0400

    Erased unnecessary Brick.java code

--- BrickBreaker/app/src/main/java/com/example/brickbreaker/Brick.java
+++ BrickBreaker/app/src/main/java/com/example/brickbreaker/Brick.java
@@ -1,16 +1,9 @@
 package com.example.brickbreaker;
 
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-
 public class Brick {
     boolean isActive;
     int height, width, x, y;
 
-    protected Brick(){
-
-    }
     protected Brick(boolean isActive, int height, int width, int x, int y){
         this.isActive = isActive;
         this.height = height;
@@ -19,8 +12,4 @@ public class Brick {
         this.y = y;
     }
 
-//    public void draw(Canvas canvas, Paint paint){
-//        paint.setColor(Color.DKGRAY);
-//        canvas.drawRect(x, y, x + width, y + height, paint);
-//    }
 }
