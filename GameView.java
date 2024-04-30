6c73435c0f3941d5ac3fc4c3427d9b538d2c6cfc
HunterDowney <88862974+HunterDowney@users.noreply.github.com>
2024-04-30 14:37:09 -0400

    Add files via upload
    Added slider movement, still experimenting and trying to solve the collision.

---
+++ GameView.java
@@ -0,0 +1,63 @@
+package com.example.brickbreaker;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+
+public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+
+    GameThread gameThread;
+
+    public GameView(Context context) {
+        super(context);
+        getHolder().addCallback(this);
+    }
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        getHolder().addCallback(this);
+    }
+
+    public GameView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        getHolder().addCallback(this);
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+        gameThread = new GameThread(holder, getWidth(), getHeight());
+        gameThread.start();
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+        gameThread.running = false;
+        boolean retry = true;
+
+        while (retry) {
+            try {
+                gameThread.join();
+                retry = false;
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            int x = (int) event.getX();
+            gameThread.setSliderX(x);
+        }
+        return true;
+    }
+}
